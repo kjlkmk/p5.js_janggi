8c1714afd16020057baae20a92bd549fa938d82c
kjlkmk <kjlkmk@gmail.com>
2024-08-16 21:23:44 +0900

    수정

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ function detectDevice() {
     createCanvas(windowWidth * 0.95, windowWidth);
   } else {
     console.log("컴퓨터에서 접속하셨습니다.");
-    createCanvas(650, 650);
+    createCanvas(600, 600);
   }
 }
 
